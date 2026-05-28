2fba0faf25fc6606a69f6c09ffb95c19f3f6c942
avikcincy-sanju <avikcincy@gmail.com>
2026-05-28 15:50:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BNPL_Cost_Conversion_Calculator/',
+
   plugins: [react()],
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
